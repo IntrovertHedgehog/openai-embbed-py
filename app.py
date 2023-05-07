3f069db708d1885c16191f3868f49f0ba287fe03
IntrovertHedgehog <larby2409@gmail.com>
2023-05-07 12:25:14 +0800

    update 2 sets of question (~100)

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from openai.embeddings_utils import cosine_similarity
 app = Flask(__name__)
 df = pd.read_csv('embbeded_question.csv')
 df['ada_embedding'] = df.ada_embedding.apply(eval).apply(np.array)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key_path = 'openai_api_key'
 
 def get_embedding(text, model="text-embedding-ada-002"):
 	text = text.replace("\n", " ")
@@ -35,5 +36,5 @@ def getTopQns():
 	)
 	return response
 
-#if __name__ == "__main__":
-#    app.run()
+if __name__ == "__main__":
+   app.run()
