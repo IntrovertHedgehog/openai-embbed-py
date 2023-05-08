672843d311836f63313443b2dff0acf892fcc86d
IntrovertHedgehog <larby2409@gmail.com>
2023-05-08 16:16:42 +0800

    tree_index

--- app.py
+++ app.py
@@ -4,23 +4,40 @@ import numpy as np
 import json
 import os
 import openai
+import logging
+import sys
+import json
+from dotenv import load_dotenv
 from openai.embeddings_utils import cosine_similarity
+from llama_index import GPTTreeIndex, StorageContext, load_index_from_storage
 
 app = Flask(__name__)
-df = pd.read_csv('embbeded_question.csv')
-df['ada_embedding'] = df.ada_embedding.apply(eval).apply(np.array)
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # openai.api_key_path = 'openai_api_key'
+print(openai.api_key)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# init embbedding
+df = pd.read_csv('embbeded_question.csv')
+df['ada_embedding'] = df.ada_embedding.apply(eval).apply(np.array)
+
+# init llama index
+persist_dir = "./tree"
+storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
+tree_index = load_index_from_storage(storage_context)
 
 def get_embedding(text, model="text-embedding-ada-002"):
 	text = text.replace("\n", " ")
 	return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
 def search_reviews(df, question, n=3):
-	embedding = get_embedding(question)
-	df['similarities'] = df.ada_embedding.apply(lambda x: cosine_similarity(x, embedding))
-	res = df.sort_values('similarities', ascending=False).head(n)
-	return res
+    embedding = get_embedding(question)
+    df['similarities'] = df.ada_embedding.apply(lambda x: cosine_similarity(x, embedding))
+    res = df.sort_values('similarities', ascending=False).head(n)
+    return res
 
 @app.route('/embbeding', methods=['POST'])
 def getTopQns():
@@ -36,5 +53,30 @@ def getTopQns():
 	)
 	return response
 
-# if __name__ == "__main__":
-#    app.run()
+@app.route('/tree_index', methods=['POST'])
+def getResTreeIndex():
+    global tree_index
+    question = request.get_json()['input']
+    if question is None:
+        return "No text found", 400
+    query_engine = tree_index.as_query_engine()
+    data={"response": query_engine.query(question).response}
+    data_json=json.dumps(data)
+    response = app.response_class(
+        response=data_json,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/test')
+def test():
+    data={"a":12}
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype="application/json"
+    )
+
+if __name__ == "__main__":
+   app.run()
